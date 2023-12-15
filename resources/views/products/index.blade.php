fae3a4c2d00a4f5ed988b58af991f2eebbb7e290
jitunmohajan <jitunmohajan@gmail.com>
2023-12-15 12:40:24 -0800

    first commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+        <title>Laravel CRUD</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-sm bg-dark">
+            <!-- Links -->
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link text-light" href="/">Products</a>
+              </li>
+              
+            </ul>
+        </nav>
+
+        <div class="container">
+            <div class="text-right">
+                <a href="products/create" class="btn btn-dark mt-2">New Product</a>
+            </div>
+        </div>
+    </body>
+</html>
