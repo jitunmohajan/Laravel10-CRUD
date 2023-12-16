e5ab879247eb284727973ef7174e3a41f0ca9909
jitunmohajan <jitunmohajan@gmail.com>
2023-12-15 22:25:27 -0800

    added update delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,13 +8,14 @@ use App\Models\Product;
 class ProductController extends Controller
 {
     public function index(){
-        return view('products.index');
+        $products=Product::get();
+        return view('products.index',['products' => $products]);
+        // return view('products.index',['products' => Product::get()]);
     }
     public function create(){
         return view("products.create");
     }
     public function store(Request $request){
-
         // validation
         $request->validate([
             'name' => 'required',
@@ -24,7 +25,7 @@ class ProductController extends Controller
 
         // upload image
         $imageName = time().'.'.$request->image->extension();
-        $request->image->move(public_path('products').$imageName);
+        $request->image->move(public_path('products'), $imageName);
 
         $product = new Product;
         $product->image = $imageName;
@@ -33,6 +34,39 @@ class ProductController extends Controller
 
         $product->save();
 
-        return back();
+        return back()->withSuccess('Product Created !!!!');
+    }
+    public function edit($id){
+        $product = Product::where('id',$id)->first();
+        return view('products.edit',['product' => $product]);
+    }
+    public function update(Request $request, $id){
+        // validation
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'image' => 'nullable|mimes:jpeg,jpg,png,gif|max:10000'
+        ]);
+
+        $product = Product::where('id',$id)->first();
+
+        if(isset($request->image)){
+            // upload image
+            $imageName = time().'.'.$request->image->extension();
+            $request->image->move(public_path('products'), $imageName);
+            $product->image = $imageName;
+        }
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+
+        $product->save();
+
+        return back()->withSuccess('Product Updated !!!!');
+    }
+    public function destroy($id){
+        $product = Product::where('id',$id)->first();
+        $product->delete();
+        return back()->withSuccess('Product Deleted!!');
     }
 }
